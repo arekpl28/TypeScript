7536630aff10f0d2bbb8e48bb30625c930521b8f
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-03-22 19:32:44 +0100

    Creating a Generic Function

--- a005Generetics/dist/app.js
+++ a005Generetics/dist/app.js
@@ -1,10 +1,6 @@
 "use strict";
-const names = [];
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('This is done!');
-    }, 2000);
-});
-promise.then(data => {
-    data.split(' ');
-});
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const mergeObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+console.log(mergeObj);
--- a005Generetics/src/app.ts
+++ a005Generetics/src/app.ts
@@ -1,12 +1,19 @@
-const names: Array<string> = []
-// names[0].split(' ')
+// const names: Array<string> = []
+// // names[0].split(' ')
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('This is done!')
-  }, 2000)
-})
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('This is done!')
+//   }, 2000)
+// })
 
-promise.then(data => {
-  data.split(' ')
-})
+// promise.then(data => {
+//   data.split(' ')
+// })
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB)
+}
+
+const mergeObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 })
+console.log(mergeObj)
